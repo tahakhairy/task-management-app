1167998320f961322a4c7a635526b7ef86b6bdbc
tahakhairy <tahakhairy@ymail.com>
2025-09-24 15:33:40 +0300

    :sparkles: app name and favicon

--- src/pages/tasks.vue
+++ src/pages/tasks.vue
@@ -89,7 +89,7 @@ function pervPage() {
         </UButton>
       </div>
 
-      <div v-if="isLoading" class="max-w-3xs">
+      <div v-if="isLoading" class="max-w-3xs my-5">
         <TaskSkeleton />
       </div>
       <div v-else class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6 my-5">
